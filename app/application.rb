f20a6dd0007e6d25a540fa26f5e9c8e1b112f2c0
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-25 15:19:14 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,9 @@ class Application
     if item.nil?
       resp.write "Item not found"
       resp.status = 404
+    else
     resp.write "#{item.price}"
+  end
   else
     resp.write "Route not found"
     resp.status = 404
