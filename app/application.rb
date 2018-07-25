eddcf057d7278e419ba55e47774464dc023a198e
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-25 15:10:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,9 +4,10 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-  if !req.path=="/items"
+  if req.path=="/items"
     resp.status = 404
   end
+  resp.finish 
 
   end
 
