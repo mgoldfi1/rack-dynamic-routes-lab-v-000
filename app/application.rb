cc4282118945dc4f474cc9d4dff6e903928301c6
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-25 15:14:01 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     req = Rack::Request.new(env)
 
   if req.path.match(/items/)
-    item_name = req.path.split("/")
+    item_name = req.path.split("/items/").last
 
   end
   resp.finish
